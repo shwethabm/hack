4200238490aa432db9a5ff50d6ec8dd79e31f249
Sneha Saj <snehasaj1999@gmail.com>
2019-10-04 11:06:47 +0530

    Update urls.py

--- raeofhope/urls.py
+++ raeofhope/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('', include('upload.urls')),
+    path('', include('pages.urls')),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
